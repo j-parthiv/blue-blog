017993dc4395ea9bef634d422697cccb694427b6
j-parthiv <parthivjani9@gmail.com>
2024-06-08 13:42:30 +0200

    refactor: change styling and use badge in postcard

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -2,20 +2,28 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { capitalizeFirstLetter } from "../utils/utils";
-
+import Badge from "./Badge";
 const PostCard = ({ post }: any) => {
   return (
-    <Link href={`/posts/${post.id}`} className="hover:-translate-y-5 duration-300">
-    <div className="h-[28em] px-4 rounded-lg">
-      <Image
-        src={"/dummy.jpeg"}
-        alt="Hero"
-        height={500}
-        width={500}
-        className="rounded-lg h-3/4"
-      />
-      <p className="text-lg py-3"> {capitalizeFirstLetter(post.title)}</p>
-    </div>
+    <Link
+      href={`/posts/${post.id}`}
+      className="hover:-translate-y-5 duration-300 px-2"
+    >
+      <div className="rounded-lg my-5">
+        <Image
+          src={"/dummy-2.jpeg"}
+          alt="Hero"
+          height={500}
+          width={500}
+          className="rounded-lg h-3/4"
+        />
+      </div>
+      <Badge title=" Anonymous - 5 April 2024" />
+      <p className="font-bold	px-1 text-lg pt-4 pb-2 mt-3"> {capitalizeFirstLetter(post.title)}</p>
+      <p className="px-1 text-md">
+        {post.body}
+      </p>
+      
     </Link>
   );
 };
