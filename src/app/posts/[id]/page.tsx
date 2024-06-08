ec9f58bf370cb46e18ee78e726580019db6b4dc3
j-parthiv <parthivjani9@gmail.com>
2024-06-08 13:36:53 +0200

    feat: use trending post section in single blog page

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -5,6 +5,7 @@ import Spinner from "@/components/Spinner";
 import { usePosts } from "@/contexts/PostsContext";
 import { capitalizeFirstLetter } from "@/utils/utils";
 import Image from "next/image";
+import TrendingPosts from "@/components/TrendingPosts";
 
 const SingleBlog = ({ params }: { params: { id: string } }) => {
   const { posts, loading, error } = usePosts();
@@ -18,21 +19,38 @@ const SingleBlog = ({ params }: { params: { id: string } }) => {
 
   if (!post) return <div>Post not found</div>;
 
+  const repeatedContent = Array(25)
+    .fill(capitalizeFirstLetter(post.body))
+    .join(" ");
+
+  const currentPostIndex = posts.findIndex((post) => post.id === postId);
+
+  const nextThreePosts = posts.slice(
+    currentPostIndex + 1,
+    currentPostIndex + 4
+  );
+
   return (
-    <section className="px-9 py-9 rounded-lg">
-      <h1 className="text-4xl font-bold text-gray-900 mb-9">
-        {capitalizeFirstLetter(post.title)}
-      </h1>
-      <Image
-        src="/dummy.jpeg"
-        alt={post.title}
-        height={500}
-        width={500}
-        className="w-full h-auto rounded mb-9"
-      />
-      <p className="text-lg text-gray-700 leading-relaxed">
-        {capitalizeFirstLetter(post.body)}
-      </p>
+    <section className="md:px-5 py-9">
+      <div className="mb-9">
+        <h1 className="text-center text-4xl font-bold mb-5 text-gray-900 xl:px[10em]">
+          {capitalizeFirstLetter(post.title)}
+        </h1>
+        <hr />
+        <div className="flex justify-center p-9">
+        <Image
+          src="/dummy.jpeg"
+          alt="dummy"
+          width={1000}
+          height={400}
+          className="rounded-xl"
+        />
+      </div>
+        <p className="text-lg rounded-2xl text-gray-700 leading-relaxed">
+          {repeatedContent}
+        </p>
+      </div>
+      <TrendingPosts posts={nextThreePosts} />
     </section>
   );
 };
