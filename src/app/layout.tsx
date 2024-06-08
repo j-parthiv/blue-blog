6cfc134a189a6f6c2b8fb71c506f5532a805f939
j-parthiv <parthivjani9@gmail.com>
2024-06-08 09:52:53 +0200

    feat: add styling and use navbar in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { PostsProvider } from "../contexts/PostsContext";
+import Navbar from "../components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <PostsProvider>
+          <main className="w-full p-5 mx-auto 2xl:px-[10em] container">
+            <Navbar />
+            {children}
+          </main>
+        </PostsProvider>
+      </body>
     </html>
   );
 }
