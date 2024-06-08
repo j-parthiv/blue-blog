53346ddd74126882d81615f4242acde0b63f4a02
j-parthiv <parthivjani9@gmail.com>
2024-06-08 13:34:45 +0200

    feat: use hero component on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 "use client";
+import Hero from "@/components/Hero";
 import Posts from "@/components/Posts";
 export default function Home() {
-  
   return (
     <>
       <section className="flex flex-col gap-5 justify-center items-center	py-7">
-        <h1 className="text-4xl font-semibold text-primary">Our Blogs</h1>
-        <p className="tracking-wide">
-          A center for all our resources & insights
-        </p>
-        <div>
-          <Posts />
-        </div>
+        <Hero />
+        <Posts />
       </section>
     </>
   );
