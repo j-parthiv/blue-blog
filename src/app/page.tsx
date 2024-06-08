c7ea069f6431ea24103949816f2032fa42cf1214
j-parthiv <parthivjani9@gmail.com>
2024-06-08 09:56:13 +0200

    feat: use posts section on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import Posts from "@/components/Posts";
 export default function Home() {
-  return <div>Hello World</div>;
+  
+  return (
+    <>
+      <section className="flex flex-col gap-5 justify-center items-center	py-7">
+        <h1 className="text-4xl font-semibold text-primary">Our Blogs</h1>
+        <p className="tracking-wide">
+          A center for all our resources & insights
+        </p>
+        <div>
+          <Posts />
+        </div>
+      </section>
+    </>
+  );
 }
