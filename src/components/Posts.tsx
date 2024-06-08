3ac3a95261b6738beb845b77136d9abf765b0309
j-parthiv <parthivjani9@gmail.com>
2024-06-08 13:43:43 +0200

    feat: change styling and use pagination and badge component in posts

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { capitalizeFirstLetter } from "../utils/utils";
 import Spinner from "./Spinner";
 import Link from "next/link";
+import Pagination from "./Pagination";
+import Badge from "./Badge";
 const Posts = () => {
   const { posts, loading, error } = usePosts();
   const [searchQuery, setSearchQuery] = useState<string>("");
@@ -39,35 +41,16 @@ const Posts = () => {
 
   return (
     <>
-      <section className="py-9">
-        <Link href={`/posts/${posts[0].id}`}>
-          <div className="flex max-xl:flex-col gap-16 py-5">
-            <Image
-              src="/dummy.jpeg"
-              alt="Hero"
-              width={500}
-              height={500}
-              layout="responsive"
-              className="rounded-lg"
-            />
-            <div className="flex flex-col justify-center">
-              <p className="flex gap-2 text-sm font-medium text-primary">
-                Trending Blog
-              </p>
-              <h1 className="mt-4 text-4xl leading-[50px] font-bold tracking-[-1.2px] text-gray-900">
-                {capitalizeFirstLetter(posts[0].title)}
-              </h1>
-            </div>
-          </div>
-        </Link>
-        <div
-          className="flex flex-col justify-center items-center py-6"
-          ref={postsContainerRef}
-        >
-          <p className="text-2xl font-semibold py-4">All Blogs</p>
+      <section className="py-9" ref={postsContainerRef}>
+        <div className=" flex flex-col justify-center items-center text-center">
+        <Badge title="Our Blogs" />
+          <h1 className="text-3xl font-bold text-gray-900 mb-4 leading-snug  my-5">
+            Explore diverse categories of our articles
+          </h1>
+          {/* <p>Discover article that inspire and motivate you to reach new heights. Explore out content for valuable insighhhts and empowering stories that will fuel your ambition, Check it out</p> */}
           <input
             type="text"
-            className="text-input w-1/2 border border-black"
+            className="w-1/2 my-5 p-3 border border-black rounded-3xl outline-none bg-transparent"
             placeholder="Search our blogs by titles"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
@@ -81,30 +64,20 @@ const Posts = () => {
               </p>
             </div>
           ) : (
-            <div className="py-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div className="py-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-center">
               {currentPosts.map((post) => (
                 <PostCard key={post.id} post={post} />
               ))}
             </div>
           )}
         </div>
-        <div className="flex justify-end space-x-2 mt-4">
-          {currentPage > 1 && (
-            <button
-              onClick={() => paginate(currentPage - 1)}
-              className="px-4 py-2 rounded bg-black text-white hover:-translate-y-1 duration-300"
-            >
-              Previous
-            </button>
-          )}
-          {indexOfLastPost < filteredPosts.length && (
-            <button
-              onClick={() => paginate(currentPage + 1)}
-              className="px-4 py-2 rounded bg-black text-white hover:-translate-y-1 duration-300"
-            >
-              Next
-            </button>
-          )}
+        <div className="flex justify-center space-x-2 mt-4">
+          <Pagination
+            currentPage={currentPage}
+            totalPosts={filteredPosts.length}
+            postsPerPage={postsPerPage}
+            paginate={paginate}
+          />
         </div>
       </section>
     </>
