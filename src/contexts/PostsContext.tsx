5a1e9a15f685513e794390f21bd43a082ab13934
j-parthiv <parthivjani9@gmail.com>
2024-06-08 09:50:53 +0200

    feat: create a context to fetch and store data

---
+++ src/contexts/PostsContext.tsx
@@ -0,0 +1,66 @@
+"use client";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  ReactNode,
+} from "react";
+import axios from "axios";
+
+export interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  }
+  
+interface PostsContextProps {
+  posts: Post[];
+  loading: boolean;
+  error: Error | null;
+}
+
+const PostsContext = createContext<PostsContextProps | undefined>(undefined);
+
+export const usePosts = (): PostsContextProps => {
+  const context = useContext(PostsContext);
+  
+  if (!context) {
+    throw new Error("usePosts must be used within a PostsProvider");
+  }
+  return context;
+};
+
+interface PostsProviderProps {
+  children: ReactNode;
+}
+
+export const PostsProvider: React.FC<PostsProviderProps> = ({ children }) => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        setPosts(response.data);
+        setLoading(false);
+      } catch (err) {
+        setError(err as Error);
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <PostsContext.Provider value={{ posts, loading, error }}>
+      {children}
+    </PostsContext.Provider>
+  );
+};
