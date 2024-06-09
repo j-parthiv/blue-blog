41f906436efbbd8fbaed832fedc8bbd96c8b58fe
j-parthiv <parthivjani9@gmail.com>
2024-06-09 21:21:48 +0200

    refactor: use post interface for props and capitalize first letter of body

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { FC } from "react";
 import { capitalizeFirstLetter } from "../utils/utils";
 import Badge from "./Badge";
-const PostCard = ({ post }: any) => {
+
+interface PostCardProps {
+  post: Post;
+}
+
+const PostCard: FC<PostCardProps> = ({ post }) => {
   return (
     <Link
       href={`/posts/${post.id}`}
-      className="hover:-translate-y-5 duration-300 px-2"
+      className="hover:-translate-y-5 duration-300 px-2 mx-3 md:mx-0"
     >
       <div className="flex justify-center rounded-lg my-5">
         <Image
-          src={"/dummy-2.jpeg"}
+          src="/dummy-2.jpeg"
           alt="Hero"
           height={500}
           width={500}
@@ -19,11 +25,10 @@ const PostCard = ({ post }: any) => {
         />
       </div>
       <Badge title=" Anonymous - 5 April 2024" />
-      <p className="font-bold	px-1 text-lg pt-4 pb-2 mt-3"> {capitalizeFirstLetter(post.title)}</p>
-      <p className="px-1 text-md">
-        {post.body}
+      <p className="font-bold	px-1 text-lg pt-4 pb-2 mt-3">
+        {capitalizeFirstLetter(post.title)}
       </p>
-      
+      <p className="px-1 text-md">{capitalizeFirstLetter(post.body)}</p>
     </Link>
   );
 };
