05e71a3033250f3cc6d9566aa95b554f9be57dc9
j-parthiv <parthivjani9@gmail.com>
2024-06-09 14:53:25 +0200

    refactor: show trending posts section on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
-"use client";
 import Hero from "@/components/Hero";
 import Posts from "@/components/Posts";
-export default function Home() {
+import TrendingPosts from "@/components/TrendingPosts";
+import { getPosts } from "@/utils/api";
+
+export interface PostProps {
+  posts: Post[];
+  loading: boolean;
+  error: any;
+}
+
+export default async function Home() {
+  const { posts, loading, error }: PostProps = await getPosts();
+  const nextThreePosts = posts?.slice(posts.length - 3, posts.length);
   return (
     <>
       <section className="flex flex-col gap-5 justify-center items-center	py-7">
         <Hero />
-        <Posts />
+        <Posts posts={posts} loading={loading} error={error} />
+        <TrendingPosts posts={nextThreePosts} />
       </section>
     </>
   );
