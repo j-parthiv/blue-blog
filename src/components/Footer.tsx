0d64113daa03ab13ee38c0d5cad63949f9e744e6
j-parthiv <parthivjani9@gmail.com>
2024-06-09 21:15:03 +0200

    feat: create a footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Footer: FC = () => {
+  return (
+    <footer className="w-full py-4 bg-gray-800 text-center">
+      <p className="text-white font-bold">Blue Avenir</p>
+    </footer>
+  );
+};
+
+export default Footer;
