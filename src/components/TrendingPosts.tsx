d35cd2ba1460d179fed05ca4202eeeafee8cfb85
j-parthiv <parthivjani9@gmail.com>
2024-06-09 14:57:16 +0200

    fix: solve undefined array error

--- src/components/TrendingPosts.tsx
+++ src/components/TrendingPosts.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import PostCard from "@/components/PostCard"; // Ensure the correct import path
-import { Post } from "@/contexts/PostsContext"; // Ensure this interface is correctly imported
+import PostCard from "@/components/PostCard";
 
 interface TrendingPostsProps {
   posts: Post[];
@@ -11,7 +10,7 @@ const TrendingPosts: React.FC<TrendingPostsProps> = ({ posts }) => {
     <div className="mt-9">
       <h2 className="text-2xl font-semibold mb-4">Trending Posts</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {posts.map((post) => (
+        {posts?.map((post) => (
           <PostCard key={post.id} post={post} />
         ))}
       </div>
